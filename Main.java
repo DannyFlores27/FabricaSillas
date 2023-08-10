9a4b0c112101983a6a2cb762177f00b80ca5e08c
DannyFlores27 <126033449+DannyFlores27@users.noreply.github.com>
2023-08-10 06:27:57 +0000

    New class Persona

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
     Material material = new Material();
+    Persona persona = new Persona();
   }
 }
---
+++ Persona.java
@@ -0,0 +1,57 @@
+public class Persona {
+  private String nombre;
+  private String apellido;
+  private String cui;
+  private String id;
+  private String puesto;
+  private Integer estado;
+
+  public String getNombre() {
+    return nombre;
+  }
+
+  public void setNombre(String nombre) {
+    this.nombre = nombre;
+  }
+
+  public String getApellido() {
+    return apellido;
+  }
+
+  public void setApellido(String apellido) {
+    this.apellido = apellido;
+  }
+
+  public String getCui() {
+    return cui;
+  }
+
+  public void setCui(String cui) {
+    this.cui = cui;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getPuesto() {
+    return puesto;
+  }
+
+  public void setPuesto(String puesto) {
+    this.puesto = puesto;
+  }
+
+  public Integer getEstado() {
+    return estado;
+  }
+
+  public void setEstado(Integer estado) {
+    this.estado = estado;
+  }
+
+}
