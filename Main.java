1970e23304f84910c9992a1ee5380ef37fd8aa70
DannyFlores27 <126033449+DannyFlores27@users.noreply.github.com>
2023-08-22 05:55:40 +0000

    Interface ejercicio programación 2.

--- Main.java
+++ Main.java
@@ -1,7 +1,5 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
-    Material material = new Material();
-    Persona persona = new Persona();
+    System.out.println("Hello prueba interface!");
   }
 }
